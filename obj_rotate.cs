6dc1d15768a7ab574c702a9de5c65ce6100dcbdb
esuone <juitigatu11@gmail.com>
2025-06-01 01:12:10 +0900

    Unityのプロジェクト内のファイルと間違えていたので修正

--- obj_rotate.cs
+++ obj_rotate.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class camera_rotate : MonoBehaviour
 {
-    [Range(0, 200)]
+    [Range(0, 240)]
     public float rotate_scale = 40f;
+
     // Start is called before the first frame update
     void Start()
     {
